c75f834b1492ce7cfb32c55dec1075f5307f201d
josesinbob <112579166+josesinbob@users.noreply.github.com>
2023-11-26 22:40:27 -0600

    Update Ejercicio01.cpp

--- PAT_HojaDeTrabajo03/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio01.cpp
@@ -1,6 +1,16 @@
 #include "Ejercicio01.h"
 
-int Ejercicio01::findMin(int nums[], int n)
-{
-    return 0;
+int Ejercicio01::findMin(int nums[], int n) {
+    int inicio = 0;
+    int fin = n - 1;
+
+    while (inicio < fin) {
+        int mitad = inicio + (fin - inicio) / 2;
+
+        (nums[mitad] > nums[fin]) ? inicio = mitad + 1 :
+        (nums[mitad] < nums[fin]) ? fin = mitad :
+        fin--;
+    }
+
+    return nums[inicio];
 }
