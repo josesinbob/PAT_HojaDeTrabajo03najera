818870a303b6d02e31c130895f69e816b46dbec4
josesinbob <112579166+josesinbob@users.noreply.github.com>
2023-11-26 22:43:59 -0600

    Update Ejercicio03.cpp

--- PAT_HojaDeTrabajo03/Ejercicio03.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio03.cpp
@@ -1,6 +1,37 @@
 #include "Ejercicio03.h"
 
-Node<int>* Ejercicio03::copyList(Node<int>* head)
-{
-    return nullptr;
+Node<int>* Ejercicio03::copyList(Node<int>* head) {
+    if (!head) {
+        return nullptr;
+    }
+
+   
+    Node<int>* current = head;
+    while (current) {
+        Node<int>* nextNode = current->next;
+        current->next = new Node<int>{current->value, nextNode, nullptr};
+        current = nextNode;
+    }
+
+ 
+    current = head;
+    while (current) {
+        if (current->random) {
+            current->next->random = current->random->next;
+        }
+        current = current->next ? current->next->next : nullptr;
+    }
+
+   
+    Node<int>* newHead = head->next;
+    current = head;
+    Node<int>* newCurrent = newHead;
+    while (current) {
+        current->next = current->next ? current->next->next : nullptr;
+        newCurrent->next = newCurrent->next ? newCurrent->next->next : nullptr;
+        current = current->next;
+        newCurrent = newCurrent->next;
+    }
+
+    return newHead;
 }
