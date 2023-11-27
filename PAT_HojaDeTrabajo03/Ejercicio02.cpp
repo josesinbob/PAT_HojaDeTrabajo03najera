f571e4207cf63c03e0733cb4aac182bfd11716ab
josesinbob <112579166+josesinbob@users.noreply.github.com>
2023-11-26 22:49:11 -0600

    Update Ejercicio02.cpp

--- PAT_HojaDeTrabajo03/Ejercicio02.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio02.cpp
@@ -4,26 +4,21 @@
 
 vector<vector<string>>* Ejercicio02::groupAnagrams(vector<string>& strings)
 {
-    std::unordered_map<std::string, std::vector<std::string>> anagramMap;
+     std::unordered_map<std::string, std::vector<std::string>> anagramMap;
 
-    
-    std::transform(strings.begin(), strings.end(), std::back_inserter(anagramMap),
-                   [](const std::string& str) {
-                       std::string sortedStr = str;
-                       std::sort(sortedStr.begin(), sortedStr.end());
-                       return std::make_pair(sortedStr, std::vector<std::string>{str});
-                   },
-                   [](const std::pair<const std::string, std::vector<std::string>>& pair) {
-                       return pair;
-                   });
+    for (const std::string& str : strings) 
+    {
+        std::string sortedStr = str;
+        std::sort(sortedStr.begin(), sortedStr.end());
 
-   
-    auto result = new vector<vector<string>>(anagramMap.size());
-    std::transform(anagramMap.begin(), anagramMap.end(), result->begin(),
-                   [](const std::pair<const std::string, std::vector<std::string>>& pair) {
-                       return pair.second;
-                   });
+        anagramMap[sortedStr].push_back(str);
+    }
 
+    auto result = new vector<vector<string>>;
+    for (const auto& pair : anagramMap) 
+    {
+        result->push_back(pair.second);
+    }
     return result;
 }
 
