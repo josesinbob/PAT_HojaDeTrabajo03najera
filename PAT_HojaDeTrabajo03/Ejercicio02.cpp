070fbf8fad44ea0a19cf8f4373558be0dbef438f
josesinbob <112579166+josesinbob@users.noreply.github.com>
2023-11-26 22:42:48 -0600

    Update Ejercicio02.cpp

--- PAT_HojaDeTrabajo03/Ejercicio02.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio02.cpp
@@ -1,6 +1,29 @@
 #include "Ejercicio02.h"
+#include <unordered_map>
+#include <algorithm>
 
 vector<vector<string>>* Ejercicio02::groupAnagrams(vector<string>& strings)
 {
-    return nullptr;
+    std::unordered_map<std::string, std::vector<std::string>> anagramMap;
+
+    
+    std::transform(strings.begin(), strings.end(), std::back_inserter(anagramMap),
+                   [](const std::string& str) {
+                       std::string sortedStr = str;
+                       std::sort(sortedStr.begin(), sortedStr.end());
+                       return std::make_pair(sortedStr, std::vector<std::string>{str});
+                   },
+                   [](const std::pair<const std::string, std::vector<std::string>>& pair) {
+                       return pair;
+                   });
+
+   
+    auto result = new vector<vector<string>>(anagramMap.size());
+    std::transform(anagramMap.begin(), anagramMap.end(), result->begin(),
+                   [](const std::pair<const std::string, std::vector<std::string>>& pair) {
+                       return pair.second;
+                   });
+
+    return result;
 }
+
